71cd63e2aa1e59888f20e4ce3db2904b25fb7fa6
tanmancan <contact@tkarimdesign.com>
2016-02-29 22:29:06 -0500

    Added breakpoint example

---
+++ breakpoints/app.js
@@ -0,0 +1,46 @@
+"user strict";
+
+var a = function() {
+		return new Promise(
+			function(resolve,reject) {
+				resolve('Hello World');
+			}
+		);
+	},
+	b = function() {
+		return new Promise(
+			function(resolve,reject) {
+				var response = {
+					a: 1,
+					b: 2,
+					c: 3,
+					d: 4
+				};
+				resolve(response);
+			}
+		);
+	},
+	c = function() {
+		return new Promise(
+			function(resolve,reject) {
+				var el = document.createElement('div');
+				el.id = 'Element';
+				el.style.background = 'red';
+				resolve(el);
+			}
+		);
+	};
+
+
+a().then(function(a) {
+	return b();
+// Add breakpoin on line below to see value returned by the Promise
+}).then(function(b) {
+	return c();
+// Add breakpoin on line below to see value returned by the Promise
+}).then(function(c) {
+	
+// Add breakpoin on line below to see value returned by the Promise
+}).catch(function(err) {
+
+});
---
+++ breakpoints/readme.md
@@ -0,0 +1,5 @@
+# Using breakpoints
+
+[More info about Chrome devtools](https://developer.chrome.com/devtools/docs/javascript-debugging)
+
+![](javascript-debugging-overview.jpg)
