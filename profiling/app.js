55d2ea3f23f57949775b2c1f6d86af92f5bdac79
tanmancan <contact@tkarimdesign.com>
2016-07-11 11:13:44 -0400

    Fixed cost add, cleaned up code

--- profiling/app.js
+++ profiling/app.js
@@ -8,7 +8,12 @@ var calculateMeetingCost = {
 	timeoutRef: null,
 	rafRef: null,
 	startTime: null,
-	totalTimeSeconds: 0,
+	totalCost: 0,
+	totalTimeSecondsCounter: 0,
+	totalTimeMinuteCounter: 0,
+	totalTimeHourCounter: 0,
+	useRaf: true,
+
 	handleEvent: function(e) {
 		var _this = this;
 		if(e.target === _this.startButton) {
@@ -17,63 +22,77 @@ var calculateMeetingCost = {
 			_this.stopCount();
 		}
 	},
+
 	startCount: function() {
 		var _this = this;
+		
 		_this.rate = document.getElementById('rate').value;
 		_this.participants = document.getElementById('participants').value;
+		
+		if(!_this.startTime)_this.startTime = Date.now();
+
+		if(_this.useRaf) {
+			_this.rafTimer();
+		}else {
+			_this.setTimeOutTimer();
+		}
 
 		console.log('start counting');
-		_this.setTimeOutTimer();
 	},
+
 	stopCount: function() {
 		var _this = this;
-		console.log('stop counting');
 		window.clearTimeout(_this.timeoutRef);
+		window.cancelAnimationFrame(_this.rafRef);
+
+		console.log('stop counting');
 	},
+
 	calcCost: function(time) {
 		var _this = this,
 			totalTime = time || 1,
-			totalTimeHour,
-			cost;
+			totalTimeHour;
 
 
 		totalTimeHour = totalTime / 3600;
-		_this.totalTimeSeconds = (Math.ceil(totalTime) % 60 === 0) ? 0 : _this.totalTimeSeconds + 1;
-		cost = totalTimeHour * _this.rate * _this.participants;
-
-		console.log('COST: ', cost);
-		console.log(totalTime, (totalTime % 60 === 0), 'totalTimeSeconds: ', _this.totalTimeSeconds);
-
-		_this.timeDisplay.innerHTML = Math.floor(time / 3600) + ':' + Math.floor(time / 60) + ':' + _this.totalTimeSeconds;
-		_this.costDisplay.innerHTML = '$' + Math.floor(cost * 100) / 100;
+		_this.totalTimeSecondsCounter = (Math.ceil(totalTime) % 60 === 0) ? 0 : _this.totalTimeSecondsCounter + 1;
+		_this.totalCost = totalTimeHour * _this.rate * _this.participants;
+		_this.timeDisplay.innerHTML = Math.floor(time / 3600) + ':' + Math.floor(time / 60) + ':' + _this.totalTimeSecondsCounter;
+		_this.costDisplay.innerHTML = '$' + Math.floor(_this.totalCost * 100) / 100;
 	},
+
 	setTimeOutTimer: function() {
 		var _this = this;
-		_this.startTime = Date.now();
 
 		function countTime() {
 			var totalTime,
-				cost,
-				currTime = Date.now(),
-				dispTime;
+				currTime = Date.now();
 
-				totalTime = (currTime - _this.startTime) / 1000;
-				dispTime = Date(currTime - _this.startTime);
-
-			console.log('time time');
-			console.log(dispTime, totalTime);
+			totalTime = (currTime - _this.startTime) / 1000;
 			_this.calcCost(totalTime);
-			_this.timeoutRef = window.setTimeout(countTime, 1000);
+			_this.timeoutRef = window.setTimeout(countTime);
 		}
-		_this.timeoutRef = window.setTimeout(countTime, 1000);
+		_this.timeoutRef = window.setTimeout(countTime);
 	},
+
 	rafTimer: function() {
 		var _this = this;
 
+		function countTime() {
+			var totalTime,
+				currTime = Date.now();
+
+			totalTime = (currTime - _this.startTime) / 1000;
+			_this.calcCost(totalTime);
+			_this.rafRef = window.requestAnimationFrame(countTime);
+		}
+		_this.rafRef = window.requestAnimationFrame(countTime);
+
 	},
+
 	init: function() {
 		var _this = this;
-		console.log(_this.timeDisplay, _this.costDisplay, _this.startButton, _this.stopButton);
+		
 		_this.startButton.addEventListener('click', _this, false);
 		_this.stopButton.addEventListener('click', _this, false);
 	}
