114e45009b40c5f9a72b0132831a55314e5861e4
tanmancan <contact@tkarimdesign.com>
2016-07-13 10:50:30 -0400

    Added additional animation

--- profiling/animate.js
+++ profiling/animate.js
@@ -8,8 +8,11 @@ var calculateMeetingCost = {
 	ballAnimationDirection: 1,
 	timeoutRef: null,
 	rafRef: null,
+	growBallRef: null,
+	ballSize: 1,
+	ballSizeModifier: .1,
 	animationState: false,
-	useRaf: true,
+	useRaf: false,
 
 	handleEvent: function(e) {
 		var _this = this;
@@ -21,36 +24,50 @@ var calculateMeetingCost = {
 	},
 
 	startAnimation: function() {
-		var _this = this;
+		var _this = this,
+			animateProps = {};
+
+		animateProps.ballBoundingBox = _this.ball.getBoundingClientRect();
+		animateProps.ballHeight = _this.ball.getBoundingClientRect().height;
+		animateProps.animationBoundingBox = _this.animationBlock.getBoundingClientRect();
+		animateProps.animationBlockHeight = _this.animationBlock.getBoundingClientRect().height;
+
+		animateProps.ballMovePct = Math.floor((animateProps.animationBlockHeight / animateProps.ballHeight - 1) * 100);
 
 		if(!_this.startTime)_this.startTime = Date.now();
 
 		if(_this.pauseButton) {
 			window.clearTimeout(_this.timeoutRef);
 			window.cancelAnimationFrame(_this.rafRef);
+			window.clearTimeout(_this.growBallRef);
+			window.cancelAnimationFrame(_this.growBallRef);
 			_this.startButton.innerHTML = 'Start Animation';
 		}else {
 			if(_this.useRaf) {
-				_this.rafTimer();
+				_this.rafTimer(animateProps);
+				_this.growBallRaf();
 			}else {
-				_this.setTimeOutTimer();
+				_this.setTimeOutTimer(animateProps);
+				_this.growBall();
 			}
 			_this.startButton.innerHTML = 'Pause Animation';
 		}
 
 		_this.pauseButton = (_this.pauseButton) ? null : true;
-
 	},
 
 	stopAnimation: function() {
 		var _this = this;
 		window.clearTimeout(_this.timeoutRef);
 		window.cancelAnimationFrame(_this.rafRef);
+		window.clearTimeout(_this.growBallRef);
+		window.cancelAnimationFrame(_this.growBallRef);
 
 		_this.pauseButton = null;
 		_this.startButton.innerHTML = 'Start Animation';
 		_this.ballTopPos = 0;
 		_this.ballAnimationDirection = 1;
+		_this.ball.style.transform = 'translate3d(0,0,0)';
 	},
 
 	animateBall: function(animateProps) {
@@ -74,37 +91,57 @@ var calculateMeetingCost = {
 		_this.animationState = false;
 	},
 
-	setTimeOutTimer: function() {
-		var _this = this,
-			animateProps = {};
+	growBall: function() {
+		var _this = this;
 
-		animateProps.ballBoundingBox = _this.ball.getBoundingClientRect();
-		animateProps.ballHeight = _this.ball.getBoundingClientRect().height;
-		animateProps.animationBoundingBox = _this.animationBlock.getBoundingClientRect();
-		animateProps.animationBlockHeight = _this.animationBlock.getBoundingClientRect().height;
+		function growAnimation() {
+			if(Math.floor(_this.ballSize * 100) / 100 === 1) {
+				_this.ballSizeModifier = .1;
+			}else if(Math.floor(_this.ballSize * 100) / 100 === 2) {
+				_this.ballSizeModifier = -.1;
+			}
+			_this.ballSize += _this.ballSizeModifier;
+			_this.ball.style.transform += ' scale(' +_this.ballSize + ')';
 
-		animateProps.ballMovePct = (animateProps.animationBlockHeight / animateProps.ballHeight - 1) * 100;
+			_this.growBallRef = window.setTimeout(growAnimation, 16.6);
+		}
+
+		growAnimation();
+	},
+
+	growBallRaf: function() {
+		var _this = this;
+
+		function growAnimation() {
+			if(Math.floor(_this.ballSize * 100) / 100 === 1) {
+				_this.ballSizeModifier = .1;
+			}else if(Math.floor(_this.ballSize * 100) / 100 === 2) {
+				_this.ballSizeModifier = -.1;
+			}
+			_this.ballSize += _this.ballSizeModifier;
+			_this.ball.style.transform += ' scale(' +_this.ballSize + ')';
+			
+			_this.growBallRef = window.requestAnimationFrame(growAnimation);
+		}
+
+		growAnimation();
+	},
+
+	setTimeOutTimer: function(animateProps) {
+		var _this = this;
 
 		function animationLoop() {
 			_this.animateBall(animateProps);
 			if(!_this.animationState) {
 				_this.animationState = true;
-				_this.timeoutRef = window.setTimeout(animationLoop, 17);
+				_this.timeoutRef = window.setTimeout(animationLoop, 16.6);
 			}
 		}
-		_this.timeoutRef = window.setTimeout(animationLoop, 17);
+		animationLoop();
 	},
 
-	rafTimer: function() {
-		var _this = this,
-			animateProps = {};
-
-		animateProps.ballBoundingBox = _this.ball.getBoundingClientRect();
-		animateProps.ballHeight = _this.ball.getBoundingClientRect().height;
-		animateProps.animationBoundingBox = _this.animationBlock.getBoundingClientRect();
-		animateProps.animationBlockHeight = _this.animationBlock.getBoundingClientRect().height;
-
-		animateProps.ballMovePct = (animateProps.animationBlockHeight / animateProps.ballHeight - 1) * 100;
+	rafTimer: function(animateProps) {
+		var _this = this;
 
 		function animationLoop() {
 			_this.animateBall(animateProps);
@@ -113,7 +150,7 @@ var calculateMeetingCost = {
 				_this.rafRef = window.requestAnimationFrame(animationLoop);
 			}
 		}
-		_this.rafRef = window.requestAnimationFrame(animationLoop);
+		animationLoop();
 
 	},
 
